20e3605958e3447619398fe9dd8239b0d2fa9da9
wnstn7671 <wnstn7671@naver.com>
2023-05-16 15:23:30 +0900

    accessToken 생성

--- src/main/java/com/ll/jwt_2023_05_16/base/jwt/JwtProvider.java
+++ src/main/java/com/ll/jwt_2023_05_16/base/jwt/JwtProvider.java
@@ -1,11 +1,16 @@
 package com.ll.jwt_2023_05_16.base.jwt;
 
+import com.ll.jwt_2023_05_16.util.Ut;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtProvider {
@@ -23,5 +28,15 @@ public class JwtProvider {
         if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
 
         return cachedSecretKey;
+    }
+    public String genToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Ut.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
     }
 }
